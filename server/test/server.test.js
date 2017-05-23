8e9e1b14206b2dc203e41be4f1032937ab2549d8
Sameep Singhania <sameepsi@gmail.com>
2017-05-23 13:29:40 +0530

    test for logout

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -294,3 +294,16 @@ describe('POST /users/login', () => {
     .end(done);
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('Should delete token and return 200 ok', (done) => {
+    supertest(app)
+    .delete('/users/me/token')
+    .set('x-auth',users[0].tokens[0].token)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toEqual({})
+    })
+    .end(done);
+  });
+});
