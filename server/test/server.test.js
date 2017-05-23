3732a71d74a80877b47ab0165bea1772343052a5
Sameep Singhania <sameepsi@gmail.com>
2017-05-23 13:09:37 +0530

    test cases for login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -254,3 +254,43 @@ describe('POST /users', () => {
     .end(done);
   });
 });
+
+describe('POST /users/login', () => {
+  it('Should return valid user and auth token for valid credentials', (done) => {
+    supertest(app)
+    .post('/users/login')
+    .send({
+      email:users[1].email,
+      password:users[1].password
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toExist();
+    })
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+
+      User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+
+  });
+
+  it('Should return 400 for invalid credentials', (done) => {
+    supertest(app)
+    .post('/users/login')
+    .send({email:'sameepsi@gmail.com', password:'sameepsinghania'})
+    .expect(400)
+    .expect((res) => {
+      expect(res.body).toEqual({})
+    })
+    .end(done);
+  });
+});
